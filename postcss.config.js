37e526c90248ee44fd79fc890a6886ae062f1be8
iamso <so@dev.so>
2017-07-24 17:03:45 +0200

    update postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,27 @@
 module.exports = {
-  plugins: [
-    require('autoprefixer')()
-  ]
+  parser: 'postcss-scss',
+  plugins: {
+    'postcss-import': {},
+    'postcss-mixins': {},
+    'postcss-for': {},
+    'postcss-each': {},
+    'postcss-cssnext': {
+      diff: true,
+      map: false,
+      remove: false,
+    },
+    'postcss-nested': {},
+    // 'postcss-discard-comments': {
+    //   removeAll: true
+    // },
+    'postcss-reporter': {
+      clearMessages: true
+    },
+    // 'autoprefixer': {},
+    'cssnano': {
+      discardComments: {
+        removeAll: true
+      }
+    }
+  }
 }
